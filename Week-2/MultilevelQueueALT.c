d0f467843d67fa0d7d11a6e1cdee097aff242a95
Aaryan Prakash <141843008+TheAaryanPrakash@users.noreply.github.com>
2025-05-19 17:05:53 +0530

    Update MultilevelQueueALT.c

--- Week-2/MultilevelQueueALT.c
+++ Week-2/MultilevelQueueALT.c
@@ -1,60 +1,74 @@
 #include <stdio.h>
 
 void main() {
-    int n, iu = 0, is = 0, i, b;
-    float wt_avg, tat_avg;
+    int numProcesses, userIndex = 0, systemIndex = 0;
+    int processType, burstTimeInput;
+    float totalWaitingTime, totalTurnaroundTime;
 
     printf("Enter number of processes: ");
-    scanf("%d", &n);
+    scanf("%d", &numProcesses);
 
-    int p[n], up[n], sp[n], ubt[n], sbt[n], bt[n], u[n], wt[n], tat[n];
+    // Declare arrays
+    int finalProcessOrder[numProcesses], userProcessIds[numProcesses], systemProcessIds[numProcesses];
+    int userBurstTimes[numProcesses], systemBurstTimes[numProcesses], finalBurstTimes[numProcesses];
+    int processTypeFlags[numProcesses]; // 1 = system, 0 = user
+    int waitingTime[numProcesses], turnaroundTime[numProcesses];
 
-    for (int j = 0; j < n; j++) {
+    // Input process type and burst time
+    for (int i = 0; i < numProcesses; i++) {
         printf("Enter System/User process (1 for System / 0 for User): ");
-        scanf("%d", &i);
-        printf("Enter burst time: ");
-        scanf("%d", &b);
+        scanf("%d", &processType);
 
-        if (i == 1) {
-            sp[is] = j + 1;
-            sbt[is++] = b;
+        printf("Enter burst time: ");
+        scanf("%d", &burstTimeInput);
+
+        if (processType == 1) {
+            systemProcessIds[systemIndex] = i + 1;
+            systemBurstTimes[systemIndex++] = burstTimeInput;
         } else {
-            up[iu] = j + 1;
-            ubt[iu++] = b;
+            userProcessIds[userIndex] = i + 1;
+            userBurstTimes[userIndex++] = burstTimeInput;
         }
     }
 
-    // Merge system processes first
-    for (int j = 0; j < is; j++) {
-        p[j] = sp[j];
-        bt[j] = sbt[j];
-        u[j] = 1;  // System
+    // First, add system processes to final arrays
+    for (int i = 0; i < systemIndex; i++) {
+        finalProcessOrder[i] = systemProcessIds[i];
+        finalBurstTimes[i] = systemBurstTimes[i];
+        processTypeFlags[i] = 1;
     }
 
-    // Then user processes
-    for (int j = 0; j < iu; j++) {
-        p[j + is] = up[j];
-        bt[j + is] = ubt[j];
-        u[j + is] = 0;  // User
+    // Then add user processes
+    for (int i = 0; i < userIndex; i++) {
+        finalProcessOrder[systemIndex + i] = userProcessIds[i];
+        finalBurstTimes[systemIndex + i] = userBurstTimes[i];
+        processTypeFlags[systemIndex + i] = 0;
     }
 
-    wt[0] = 0;
-    tat[0] = bt[0];
-    wt_avg = wt[0];
-    tat_avg = tat[0];
+    // Calculate waiting time and turnaround time
+    waitingTime[0] = 0;
+    turnaroundTime[0] = finalBurstTimes[0];
+    totalWaitingTime = waitingTime[0];
+    totalTurnaroundTime = turnaroundTime[0];
 
-    for (int j = 1; j < n; j++) {
-        wt[j] = wt[j - 1] + bt[j - 1];
-        tat[j] = wt[j] + bt[j];
-        wt_avg += wt[j];
-        tat_avg += tat[j];
+    for (int i = 1; i < numProcesses; i++) {
+        waitingTime[i] = waitingTime[i - 1] + finalBurstTimes[i - 1];
+        turnaroundTime[i] = waitingTime[i] + finalBurstTimes[i];
+        totalWaitingTime += waitingTime[i];
+        totalTurnaroundTime += turnaroundTime[i];
     }
 
-    printf("Process\tSystem/User\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int j = 0; j < n; j++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[j], u[j], bt[j], wt[j], tat[j]);
+    // Output results
+    printf("\nProcess\tType\t\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("%d\t%s\t\t%d\t\t%d\t\t%d\n",
+               finalProcessOrder[i],
+               (processTypeFlags[i] == 1) ? "System" : "User",
+               finalBurstTimes[i],
+               waitingTime[i],
+               turnaroundTime[i]);
     }
 
-    printf("\nAverage waiting time = %.2f", wt_avg / n);
-    printf("\nAverage turnaround time = %.2f\n", tat_avg / n);
+    printf("\nAverage waiting time = %.2f", totalWaitingTime / numProcesses);
+    printf("\nAverage turnaround time = %.2f\n", totalTurnaroundTime / numProcesses);
 }
